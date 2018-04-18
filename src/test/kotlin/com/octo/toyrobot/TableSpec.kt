6e9f5ea797dcebe7a374a36ada2a9ba331354329
Bertrand <bertrandlf@gmail.com>
2018-04-18 15:36:35 +0200

    test table width and height

--- src/test/kotlin/com/octo/toyrobot/TableSpec.kt
+++ src/test/kotlin/com/octo/toyrobot/TableSpec.kt
@@ -6,12 +6,15 @@ import org.jetbrains.spek.api.dsl.it
 import org.junit.Assert.assertEquals
 
 
-object TableSpec: Spek({
+object TableSpec : Spek({
 
     given("a table") {
         val table = Table()
-        it("should have a height of 5"){
+        it("should have a height of 5") {
             assertEquals(5, table.height)
+        }
+        it("should have a width of 5") {
+            assertEquals(5, table.width)
         }
     }
 
