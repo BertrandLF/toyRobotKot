f5fdb9e0594c72b8c3caafbac12ec1a3605b58c4
Bertrand <bertrandlf@gmail.com>
2018-04-19 11:41:25 +0200

    a world only needs a table, places the robot

--- src/main/kotlin/com/octo/toyrobot/World.kt
+++ src/main/kotlin/com/octo/toyrobot/World.kt
@@ -1,6 +1,18 @@
 package com.octo.toyrobot
 
-class World constructor(private val robot: Robot){
+class World constructor(private val table: Table = Table()){
+
+    private var robot: Robot? = null
+
+    fun getRobot(): Robot? { return this.robot }
+
+    fun place(facing: Direction, x: Int, y: Int) {
+        this.robot = Robot(facing, x, y)
+    }
+
+    fun moveRobot() {
+        this.robot = this.robot?.move()
+    }
 
     fun report(): String {
         return robot.toString()
--- src/test/kotlin/com/octo/toyrobot/WorldSpec.kt
+++ src/test/kotlin/com/octo/toyrobot/WorldSpec.kt
@@ -7,12 +7,25 @@ import org.junit.Assert.assertEquals
 
 object WorldSpec : Spek({
 
-    given("a world with a robot") {
-        val robot = Robot(Direction.NORTH, 0, 0)
-        val world = World(robot)
+    val robot = Robot(Direction.NORTH)
+    val table = Table()
+
+    given("a world with a table") {
+        val world = World(table)
+
+        it("should place a robot on the table") {
+            world.place(Direction.NORTH, 0, 0)
+            assertEquals(robot, world.getRobot())
+        }
+
+        it("should move the robot") {
+            world.moveRobot()
+            assertEquals(Robot(Direction.NORTH, 0, 1), world.getRobot())
+        }
 
         it("should report the robot's position") {
-             assertEquals("Robot(facing=NORTH, x=0, y=0)", world.report())
+            assertEquals("Robot(facing=NORTH, x=0, y=1)", world.report())
         }
     }
+
 })
